a826ded685a43e4eb259c4d5eaf380300caa8a66
Michael Megliola <michael.megliola@gmail.com>
2018-08-02 14:10:06 -0400

    Initial upload of setup doc and test code

---
+++ esc_test.py
@@ -0,0 +1,26 @@
+import time
+import Adafruit_PCA9685
+
+pwm = Adafruit_PCA9685.PCA9685()
+
+esc_min = 150  # throttle 0
+esc_max = 320  # throttle 30%
+
+pwm.set_pwm_freq(60)
+
+for i in range(4):
+    pwm.set_pwm(i, 0, esc_min) #arm ESCs
+
+
+for i in range(4):
+    print('Setting throttle to 0%'.format(esc_min))
+    pwm.set_pwm(i, 0, esc_min)
+    time.sleep(1)
+    print('Setting throttle to 100%'.format(esc_max))
+    pwm.set_pwm(i, 0, esc_max)
+    time.sleep(1)
+    print('Setting throttle to 0%'.format(esc_min))
+    pwm.set_pwm(i, 0, esc_min)
+    time.sleep(1)
+
+
---
+++ lidar.py
@@ -0,0 +1,11 @@
+import time
+import VL53L1X
+
+tof = VL53L1X.VL53L1X(i2c_bus=1, i2c_address=0x29)
+tof.open() # Initialise the i2c bus and configure the sensor
+tof.start_ranging(1) # Start ranging, 1 = Short Range, 2 = Medium Range, 3 = Long Range
+
+while True:
+        distance_in_mm = tof.get_distance() # Grab the range in mm
+        print('Distance (mm): {0}'.format(distance_in_mm))
+        time.sleep(1)
